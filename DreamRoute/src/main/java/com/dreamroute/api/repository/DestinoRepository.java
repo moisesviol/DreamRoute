e14321623849bd86d76d4327d47e03922a881955
Moisés Viol <moises.felipe.contato@gmail.com>
2026-03-16 16:13:19 -0300

    Delete DreamRoute directory

--- DreamRoute/src/main/java/com/dreamroute/api/repository/DestinoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dreamroute.api.repository;
-
-import com.dreamroute.api.model.Destino;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DestinoRepository extends JpaRepository<Destino, Long> {
-}
