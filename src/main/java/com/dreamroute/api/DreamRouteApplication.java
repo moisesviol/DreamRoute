1a169b06a934902af0ff9b0cdbf4c0483b8d7887
Moisés Viol <moises.felipe.contato@gmail.com>
2026-03-16 16:14:04 -0300

    Add files via upload

---
+++ src/main/java/com/dreamroute/api/DreamRouteApplication.java
@@ -0,0 +1,13 @@
+package com.dreamroute.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamRouteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamRouteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/dreamroute/api/repository/DestinoRepository.java
@@ -0,0 +1,7 @@
+package com.dreamroute.api.repository;
+
+import com.dreamroute.api.model.Destino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DestinoRepository extends JpaRepository<Destino, Long> {
+}
